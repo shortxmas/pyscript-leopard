f61661c66293da1abfa7aa3dcb60565b8891dc99
Leighton Miguel <leim56@hawaii.rr.com>
2024-07-17 17:37:08 -1000

    Switch project to TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
 import React, { useState, useEffect, useRef } from "react";
 import { EditorView } from "codemirror";
 import { EditorState } from "@codemirror/state";
-import { languageServer } from "codemirror-languageserver";
 import { basicSetup } from "codemirror";
 import { python } from "@codemirror/lang-python";
 import { lintGutter } from "@codemirror/lint";
 import { indentationMarkers } from "@replit/codemirror-indentation-markers";
 
-function App() {
-  const editor = useRef(null);
-  const viewRef = useRef(null);
+const App: React.FC = () => {
+  const editor = useRef<HTMLDivElement>(null);
+  const viewRef = useRef<EditorView | null>(null);
 
   useEffect(() => {
     const serverUri = "ws://localhost:4600";
-
-    const ls = languageServer({
-      serverUri,
-      rootUri: "file:///",
-      documentUri: "file:///index.js",
-      languageId: "python",
-    });
 
     const startState = EditorState.create({
       doc: "# Write your Python code here",
       extensions: [
         basicSetup,
-        // ls,
         python(),
         lintGutter(),
         indentationMarkers(),
@@ -34,21 +25,10 @@ function App() {
 
     const view = new EditorView({
       state: startState,
-      parent: editor.current,
+      parent: editor.current!,
     });
 
     viewRef.current = view;
-
-    // if (!functionsDeclared) {
-    //   const functions = `
-    //   const mockPatchApiFunction = () => {
-    //     window.alert("Mock API function invoked.");
-    //   };
-    //   `;
-
-    //   document.getElementById("imports").textContent = functions;
-    //   setFunctionsDeclared(true);
-    // }
 
     return () => {
       view.destroy();
@@ -60,8 +40,8 @@ function App() {
 
     const newScript = document.createElement("script");
     newScript.type = "py";
-    newScript.text = imports + viewRef.current.state.doc.toString();
-    document.getElementById("result").appendChild(newScript);
+    newScript.text = imports + viewRef.current!.state.doc.toString();
+    document.getElementById("result")?.appendChild(newScript);
   };
 
   return (
@@ -69,10 +49,9 @@ function App() {
       <h1>Python Code Editor</h1>
       <div ref={editor}></div>
       <button onClick={rerunScript}>Run Script</button>
-      {/* <script id="imports"></script> */}
       <div id="result"></div>
     </div>
   );
-}
+};
 
 export default App;
